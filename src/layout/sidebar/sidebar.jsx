5a39f13c078ca7d60e05b784a4665d149c19016a
Fernando Petroli <fernando@tectonica.co>
2024-09-03 09:59:33 -0300

    update dashboard sidebar for mobile

--- src/layout/sidebar/sidebar.jsx
+++ src/layout/sidebar/sidebar.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 
 import './index.scss';
 
 const Sidebar = ({ links = [], activeItem, setActiveItem }) => {
+
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible((prev) => !prev);
+  };
+
   return (
-    <nav className="app-sidebar d-md-block bg-light sidebar collapse">
+    <nav className={`app-sidebar d-md-block bg-light sidebar collapse ${isSidebarVisible ? 'show' : ''}`}>
       <div className="pt-3 sidebar-fixed">
         <ul className="app-nav">
-          <span className="logo" onClick={() => setActiveItem(-1)}>
+          <span className="logo" onClick={() => { setActiveItem(-1); toggleSidebar(); }}>
             Library of Components
           </span>
 
@@ -21,7 +28,7 @@ const Sidebar = ({ links = [], activeItem, setActiveItem }) => {
               <li
                 className={`nav-item ${activeItem === index ? 'active' : ''}`}
                 key={`${link.to}-${index}`}
-                onClick={() => setActiveItem(index)}
+                onClick={() => { setActiveItem(index); toggleSidebar(); }}
               >
                 <ReactSVG src={link.icon} />
                 {link.label}
